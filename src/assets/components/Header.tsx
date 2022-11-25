74bce4feb0ce7f6c56895e2e92b8ed4fda5da2f9
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-25 17:54:31 -0600

    begun creating mobile menu

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -5,9 +5,20 @@ const Header = () => {
   return (
     <div className='w-full h-screen header-bg'>
       <div className='flex justify-between items-center p-8'>
-        <img src={Logo} alt='logo'/>
-        <FaBars size={22} className='text-white cursor-pointer' />
+        <a href='/'>
+          <img src={Logo} alt='logo'/>
+        </a>
+        <ul className='hidden min-[565px]:flex items-center'>
+          <li>About</li>
+          <li>Services</li>
+          <li>Projects</li>
+          <button>Contact</button>
+        </ul>
+        <FaBars size={22} className='text-white cursor-pointer min-[566px]:hidden' />
       </div>
+
+
+
     </div>
   )
 }
