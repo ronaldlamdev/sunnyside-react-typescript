1a998c0af8890e59f9cde7253d9618b577c1726c
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-26 21:45:03 -0600

    finished styling mobile menu

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -22,15 +22,15 @@ const Header = () => {
           <li>Projects</li>
           <button>Contact</button>
         </ul>
-        <FaBars onClick={handleMobileNav} size={22} className='text-white cursor-pointer min-[566px]:hidden' />
-        <div className='absolute flex flex-col justify-center items-center w-full max-w-[310px] h-[400px] top-24 mx-auto left-0 right-0'>
-          <div className={(mobileNav ? 'block ' : 'hidden ') + 'right-triangle absolute top-0 self-end right-0'} />
-          <div className={(mobileNav ? 'block' : 'hidden ') + 'absolute top-8 left-0 right-0 mx-auto w-full max-w-[310px] h-[300px] bg-white shadow-lg rounded-b'}>
+        <FaBars onClick={handleMobileNav} size={22} className='text-white cursor-pointer min-[565px]:hidden' />
+        <div className='absolute flex min-[565px]:hidden flex-col justify-start w-full max-w-[310px] min-[480px]:max-w-[400px] h-[330px] top-20 mx-auto left-0 right-0'>
+          <div className={(mobileNav ? 'block ' : 'hidden ') + 'right-triangle  self-end'} />
+          <div className={(mobileNav ? 'block' : 'hidden ') + 'mx-auto w-full h-[300px] bg-white shadow-lg rounded-b rounded-tl'}>
           <ul className='w-full h-full flex flex-col items-center justify-evenly'>
-            <li>About</li>
-            <li>Services</li>
-            <li>Projects</li>
-            <button>Contact</button>
+            <li className='cursor-pointer font-["Barlow"] text-[#5a636c]'>About</li>
+            <li className='cursor-pointer font-["Barlow"] text-[#5a636c]'>Services</li>
+            <li className='cursor-pointer font-["Barlow"] text-[#5a636c]'>Projects</li>
+            <button className='bg-[#fad400] px-6 py-3 rounded-3xl font-["Fraunces"]'>Contact</button>
           </ul>
         </div>
         </div>
