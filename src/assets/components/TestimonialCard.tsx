561a83fafd5eebddef056d779643055e144f42f8
milessteamaccount <ronaldlamdev@gmail.com>
2022-12-05 20:57:17 -0600

    styled testimonials component for larger screen sizes

--- src/assets/components/TestimonialCard.tsx
+++ src/assets/components/TestimonialCard.tsx
@@ -8,13 +8,13 @@ type CardProp = {
 const TestimonialCard = ({pfp, quote, name, position}:CardProp) => {
   return (
     <div className='flex flex-col items-center text-center gap-8'>
-      <img className='rounded-full w-[75px] h-[75px]' src={pfp} alt={name} />
-      <p className='font-["Barlow"] text-[#5a636c] px-8'>
+      <img className='rounded-full w-[75px] min-[565px]:w-[100px] h-[75px] min-[565px]:h-[100px] lg:w-[150px] lg:h-[150px]' src={pfp} alt={name} />
+      <p className='font-["Barlow"] text-[#5a636c] min-[565px]:text-xl lg:text-2xl px-8 md:px-20 lg:px-8'>
         {quote}
       </p>
       <div>
-        <h1 className='font-["Fraunces"] text-xl mb-2'>{name}</h1>
-        <h2 className='font-["Barlow"] text-[#a7abae]'>{position}</h2>
+        <h1 className='font-["Fraunces"] text-xl min-[565px]:text-2xl lg:text-3xl mb-2'>{name}</h1>
+        <h2 className='font-["Barlow"] text-[#a7abae] min-[565px]:text-xl lg:text-2xl'>{position}</h2>
       </div>
     </div>
   )
--- src/assets/components/Testimonials.tsx
+++ src/assets/components/Testimonials.tsx
@@ -3,8 +3,8 @@ import { testimonials } from '../data/data';
 
 const Testimonials = () => {
   return (
-    <div className="w-full flex flex-col h-screen lg:justify-evenly items-center py-8">
-      <h1 className='font-["Fraunces"] text-[#a7abae] text-xl tracking-[.2rem] px-8'>
+    <div className="w-full flex flex-col h-screen lg:justify-evenly items-center py-4">
+      <h1 className='font-["Fraunces"] text-[#a7abae] text-xl lg:text-4xl tracking-[.2rem] px-8 min-[1709px]:mt-48'>
         Client Testimonials
       </h1>
       <div className="flex pt-20 lg:pt-0 flex-col items-center gap-16 lg:grid lg:grid-cols-3">
